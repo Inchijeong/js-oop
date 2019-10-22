d8c36b40aa6988c51e75292715e3fff5a781ba34
Inchijeong <cde0409@naver.com>
2019-10-23 00:35:48 +0900

    Apply to Person in prototype-inheritance.js

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -9,3 +9,29 @@ console.log('subObj.superVal =>', subObj.superVal); // subObj.superVal => super
 subObj.superVal = 'sub';
 console.log('superObj.superVal =>', superObj.superVal); // superObj.superVal => super
 
+var kim = {
+    name:'kim',
+    first:10, second:20,
+    sum:function(){
+        return this.first+this.second
+    }
+}
+// var lee = {
+//     name:'lee',
+//     first:10, second: 10,
+//     avg:function(){
+//         return (this.first+this.second)/2;
+//     }
+// }
+// lee.__proto__ = kim;
+
+var lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 10;
+lee.avg = function(){
+    return (this.first+this.second)/2;
+}
+console.log('kim.sum() : ', kim.sum());
+console.log('lee.sum() : ', lee.sum());
+console.log('lee.avg() : ', lee.avg());
